288fb2d2bd4cf545871d184aa84a52223f15b3f5
shayff <shayff@users.noreply.github.com>
2020-12-22 22:32:40 +0200

    finish auth

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -16,6 +16,13 @@ module.exports = (app) => {
     );
 
     app.get(
+        '/api/logout',
+        (req, res) => {
+            req.logout();
+            res.send(req.user);
+        });
+
+    app.get(
         '/api/current_user',
         (req, res) => {
             res.send(req.user);
